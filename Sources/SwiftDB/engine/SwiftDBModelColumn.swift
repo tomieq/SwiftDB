66f734b66fd4770cdfb63f3284487c190e05784a
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-01-05 08:43:08 +0100

    Implement SwiftDBModelColumn

---
+++ Sources/SwiftDB/engine/SwiftDBModelColumn.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftDBModelColumn.swift
+//  
+//
+//  Created by Tomasz Kucharski on 05/01/2021.
+//
+
+import Foundation
+
+struct SwiftDBModelColumn {
+    let name: String
+    let dataType: String
+}
